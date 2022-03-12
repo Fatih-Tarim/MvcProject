a4f27d4545f4ac66181e566e4cc16d1b59bcd224
Fatih-Tarim <fatihtrm99@gmail.com>
2022-03-12 20:14:43 +0300

    ICategoryService, EfDal

---
+++ MvcProject/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetList();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBl(p);
+            return RedirectToAction("GetCategoryList");
+        }
+    }
+}
