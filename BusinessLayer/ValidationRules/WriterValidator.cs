f07f0281d5abfe7749d9845afd0948779829d30d
Fatih-Tarim <fatihtrm99@gmail.com>
2022-03-16 14:47:12 +0300

    Writer migration, AddWriter

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adını Boş Bırakamazsınız.");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar Soyadını Boş Bırakamazsınız.");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkımda Kısmını Boş Bırakamazsınız.");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Yazar Adı 2 karakterden küçük olamaz.");
+            RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("Yazar adı 50 karakterden büyük olamaz");
+        }
+    }
+}
