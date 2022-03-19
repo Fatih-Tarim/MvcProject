1d9fcfeb4f14b7286678a23bdfca1a5aadfae0b7
Fatih-Tarim <fatihtrm99@gmail.com>
2022-03-19 21:51:13 +0300

    About

---
+++ MvcProject/Controllers/AboutController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+
+namespace MvcProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager abm = new AboutManager(new EfAboutDal());
+
+        public ActionResult Index()
+        {
+            var aboutValues = abm.GetList();
+            return View(aboutValues);
+        }
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddAbout(About about)
+        {
+            abm.AboutAddBl(about);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
