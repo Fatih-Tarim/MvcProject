bfa95323bf7d2a959a5eb58f2bb08e6c08caf686
Fatih-Tarim <fatihtrm99@gmail.com>
2022-03-23 18:53:31 +0300

    Gallery

---
+++ MvcProject/Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProject.Controllers
+{
+    public class GalleryController : Controller
+    {
+        ImageFileManager imageManager = new ImageFileManager(new EfImageFileDal());
+        public ActionResult Index()
+        {
+            var values = imageManager.GetList();
+            return View(values);
+        }
+    }
+}
